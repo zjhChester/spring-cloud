7b895628515d4d3a77afdffad8f4b5d983f1f487
zjhChester <358737741@qq.com>
2020-07-06 17:40:30 +0800

    新增config-server读取git的配置文件

---
+++ java/config-client/src/main/java/cn/zjh/spring/configclient/ConfigClientApplication.java
@@ -0,0 +1,24 @@
+package cn.zjh.spring.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
